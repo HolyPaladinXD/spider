9f57999f6192b4b894ff4ce3a463cde908654b9d
Denis <den1sflucker@gmail.com>
2025-06-13 00:29:04 +0300

    6 questions done

--- spider.cpp
+++ spider.cpp
@@ -1,7 +1,17 @@
 #include "spider.h"
 
-Widget::Widget(QWidget *parent)
-    : QWidget(parent)
-{}
+Widget::Widget(QWidget *parent): QWidget(parent){
+    Qt::WindowFlags x = this->windowFlags() & Qt::WindowType_Mask;
+    x|= Qt::WindowCloseButtonHint | Qt::CustomizeWindowHint | Qt::WindowMinMaxButtonsHint;
+    this->setWindowFlags(x);
+
+    this->setWindowState(Qt::WindowActive);
+
+    this->setWindowTitle(QStringLiteral("Паук"));
+
+    //QIcon icon = this->windowIcon();
+
+    qDebug() << this->windowIcon();
+}
 
 Widget::~Widget() {}
